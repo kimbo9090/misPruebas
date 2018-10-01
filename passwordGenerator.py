4bccaabe1062ce893ea95463fe13e57f64dd6505
kimbo9090 <tomas9080@gmail.com>
2018-10-01 12:49:58 +0200

    Comentaros añadidos

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 import random
 import string
 numbers = [1,2,3,4,5,6,7,8,9,0]
@@ -6,20 +8,27 @@ ABC = string.ascii_uppercase
 
 def passGen():
     password = []
-    for i in range(0,9):
+    for i in range(0,12):
         number_or_letter = random.randrange(0,2)
         if number_or_letter == 1:
+            # Si es 1, asigna una letra aleatoria
             upper_or_lower = random.randrange(0,2)
+                # Se asigna otro numero aleatorio para elegir entre mayusculas o minuscula
             if upper_or_lower == 1:
+                    #Si es 1, sera minuscula.
                 password.append(random.choice(abc))
             else:
+                    #Si es 2, sera mayúscula.
                 password.append(random.choice(ABC))
+            # Si es 2, asigna un numero aleatorio
         else:
             password.append(random.choice(numbers))
+    #El resultado se une en un String
     password = ''.join(map(str,password))
     return password
 
 generated_passwords = []
 for i in range(0,5):
+    # Generamos 5 contraseñas aleatorias
     generated_passwords.append(passGen())
 print generated_passwords
